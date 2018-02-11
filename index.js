3b50f0b9dc4d90b43c9250f31c4d8111de1d8f50
Christian Robards <christianrobards42@gmail.com>
2018-02-11 20:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greeting + specialGuest + "!";
+var greetspeacialguest= + specialGuest + "!";
 
 var topic = "space";
 
